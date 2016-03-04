b6b4ae21c263de7fdaf16ea1f543bcaa59578324
nischayv <nischayv@iastate.edu>
2016-03-04 01:44:04 -0600

    api service method created to fetch all data from third party api.

--- src/main/java/com/example/dto/StoryList.java
+++ src/main/java/com/example/dto/StoryList.java
@@ -1,8 +1,18 @@
 package com.example.dto;
 
+import com.example.entity.Story;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class StoryList {
 
+    private List<Story> results;
+    private String query;
+
+    StoryList() {
+        results = new LinkedList<>();
+    }
 }
--- src/main/java/com/example/service/ApiServiceImpl.java
+++ src/main/java/com/example/service/ApiServiceImpl.java
@@ -1,10 +1,15 @@
 package com.example.service;
 
+import com.example.dto.StoryList;
 import com.example.repo.StoryRepo;
 import com.example.service.interfaces.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ApiServiceImpl implements ApiService{
@@ -15,5 +20,10 @@ public class ApiServiceImpl implements ApiService{
     @Override
     public void loadAllStories(String interestName) {
         RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> map = new HashMap<>();
+        map.put("interestName", interestName);
+        map.put("key","AjFzRJttLvOXBpb3BLsR@Qq-yp8_");
+        String url = "http://www.faroo.com/api?q={interestName}&start=1&length=10&l=en&src=news&i=false&f=json&key={key}";
+        ResponseEntity<StoryList> StoryListEntity = restTemplate.getForEntity(url, StoryList.class, map);
     }
 }

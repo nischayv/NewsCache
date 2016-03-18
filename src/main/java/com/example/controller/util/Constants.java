d64f943dff756037a269957059b6190094891fff
nischayv <nischayv@iastate.edu>
2016-03-17 21:12:19 -0500

    Removed the api key

--- src/main/java/com/example/controller/util/Constants.java
+++ src/main/java/com/example/controller/util/Constants.java
@@ -3,7 +3,8 @@ package com.example.controller.util;
 
 public class Constants {
 
-    private static final String farooKey = "AjFzRJttLvOXBpb3BLsR@Qq-yp8_";
+    //Add the Faroo API Key here
+    private static final String farooKey = "";
     private static final String farooUrl  = "http://www.faroo.com/api?q={interestName}&start=1&length=10&l=en&src=news&i=false&f=json&key={key}";
 
     public static String getFarooKey() {

4550ced164c14c7166e4f124667068d95d48499b
nischayv <nischayv@iastate.edu>
2016-04-28 20:23:15 -0500

    removed api key

--- src/main/java/com/example/controller/util/Constants.java
+++ src/main/java/com/example/controller/util/Constants.java
@@ -4,7 +4,7 @@ package com.example.controller.util;
 public class Constants {
 
     //TODO Encrypt the key. Very unsafe manner to have the key.
-    private static final String farooKey = "tCW9Wy@SSgRqeZ-FhUa02fqdI8c_";
+    private static final String farooKey = "Api key goes here";
     private static final String farooUrl  = "http://www.faroo.com/api?q={interestName}&start=1&length=10&l=en&src=news&i=false&f=json&key={key}";
 
     public static String getFarooKey() {
